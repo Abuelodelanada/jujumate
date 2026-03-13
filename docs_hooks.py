95265d2b4b6f54e6a09ce4799007b6c68b9c6d07
Jose C. Massón <jose.masson@canonical.com>
2026-03-13 00:18:09 -0300

    fix web

--- docs_hooks.py
+++ docs_hooks.py
@@ -8,3 +8,4 @@ def on_post_build(config: dict) -> None:
     site_dir = config["site_dir"]
     shutil.copy("landing/index.html", os.path.join(site_dir, "index.html"))
     shutil.copy("landing/landing.css", os.path.join(site_dir, "landing.css"))
+    shutil.copy("icon.svg", os.path.join(site_dir, "icon.svg"))
